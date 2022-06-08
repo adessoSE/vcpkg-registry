5feec76b40beacd1ad7f58249321815cf510def6
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-06-08 12:57:59 +0200

    [vefs] Add version @0.5.0-alpha.3

--- ports/vefs/portfile.cmake
+++ ports/vefs/portfile.cmake
@@ -2,15 +2,16 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL ssh://git@mdk-bs-dev02.test-server.ag:2224/native/vefs.git
-    REF 9c9c5f89a78a48a8ea31fd9dfc5e8c9d373dce97 # v0.5.0-alpha.2
+    REF 8859616bf89f5cfe89d873fde06a46f053965274 # v0.5.0-alpha.3
     HEAD_REF master
 )
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/vefs)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/vefs RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
