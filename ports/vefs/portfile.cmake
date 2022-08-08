4ae46eba1ea68e6462f2865a635b90476b6dfab3
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2022-08-08 18:11:50 +0200

    [vefs] Add version @0.5.0-beta.1

--- ports/vefs/portfile.cmake
+++ ports/vefs/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL ssh://git@mdk-bs-dev02.test-server.ag:2224/native/vefs.git
-    REF 8859616bf89f5cfe89d873fde06a46f053965274 # v0.5.0-alpha.3
+    REF 801d79d20c3d4a3ec111858de0d92a5410b88214 # v0.5.0-beta.1
     HEAD_REF master
 )
 vcpkg_cmake_configure(
