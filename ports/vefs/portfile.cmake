ff4ce77fe4834777a730e695a61652a72f4b8eb3
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-07-06 13:24:53 +0200

    [vefs] Add version @0.5.0-beta.4

--- ports/vefs/portfile.cmake
+++ ports/vefs/portfile.cmake
@@ -2,16 +2,18 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL ssh://git@mdk-bs-dev02.test-server.ag:2224/native/vefs.git
-    REF a03c47ce3fe76255acb7b445539cb5702a94721d # v0.5.0-beta.3
+    REF ee34d0f2cc3ef2bd880b59733efbbfa1207fdc3b # v0.5.0-beta.4
     HEAD_REF master
 )
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include
+        -DBUILD_TESTING=OFF
+        -DWARNINGS_AS_ERRORS=OFF
+        "-DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include"
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
