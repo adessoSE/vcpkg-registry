b6f5a9172133ec28eed9adf69b66bb57ca3f5500
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-08-25 13:57:05 +0200

    [vefs] Add version 0.5.0-beta.5

--- ports/vefs/portfile.cmake
+++ ports/vefs/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "adessoSE/libvefs"
     REF "v${VERSION}"
-    SHA512 2fecbd8e90ad5132a75efaecb42658a622f05b6318accfb4dc2ead660b54cbe94f88b039b2bb38b685e02db653a789825398af1279cf339d9fbf9ef03ead1797
+    SHA512 a3a3e7e9d77c8f5c056d3905eed26e2b2e34109dcd314e66d0fcecc361d2b90a7d2c14c9b6f8c0271321d1bff797f325e820228885c5a48457cee2b027d260fb
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
